d8bbab5ab14f25deb8ea3a27f3f5d7f5ff25aa3a
luish198 <luish198@hotmail.com>
2021-05-15 17:34:11 +0200

    Second Week HTML connection

--- index.js
+++ index.js
@@ -6,48 +6,149 @@
 */
 
 let toDoList = [];
-/*let toDoInput = "";*/
+let toDoInput = "";
 
-/* let itemList = {
+let idCounter = 0;
+
+/*let itemList = {
   task: toDoInput,
-  id: toDoList.length,
+  /*id: counterUpdate(idCounter),*/
+/*id: idCounter,
   done: false
-}; */
+};*/
 
-/*let idCounter = 0;
-let counterUpdate = (item) => {
-  toDoList.map((item) => (item.id = idCounter++));
-  return idCounter;
+let addToDoListItem = (task) => {
+  toDoList.push({ id: idCounter++, task: task, done: false });
 };
 
-console.log(counterUpdate(idCounter));*/
+//update HTML function
+const updateHTML = () => {
+  let htmlTasks = toDoList
+    .map((item, i) => {
+      return `<li id="${item.id}">
+      <input ${item.done && "checked"} type ="checkbox" class="check" id="${
+        item.id
+      }" />
+      ${item.done ? `<strike>${item.task}</strike>` : item.task}
+      <button class="delete-item" id="${item.id}">&#128465;</button>
+      </li>`;
+    })
+    .join("");
 
-/*console.log(counterupdate("this ..." + idCounter));*/
+  document.querySelector("ul").innerHTML = htmlTasks;
 
-let addToDoListItem = (task, idCounter) => {
-  toDoList.push({ id: toDoList.length, task: task, done: false });
+  //checkbox marked
+
+  const checkButtons = document.querySelectorAll(".check");
+  checkButtons.forEach((btn) =>
+    btn.addEventListener("click", (event) => {
+      /*crossItem(parseInt(event.target.id));*/
+      console.log(toDoList);
+      console.log(event.target.id);
+      toDoList = toDoList.map((item) => {
+        return item.id === parseInt(event.target.id)
+          ? { id: item.id, task: item.task, done: !item.done }
+          : item;
+      });
+      updateHTML();
+    })
+  );
+
+  const deleteButtons = document.querySelectorAll(".delete-item");
+  deleteButtons.forEach((btn) =>
+    btn.addEventListener("click", (event) => {
+      deleteItem(parseInt(event.target.id));
+      updateHTML();
+    })
+  );
 };
 
-addToDoListItem("clean bathroom");
-addToDoListItem("finish phd");
-addToDoListItem("watch TV");
-addToDoListItem("finish JavaScript project");
+//adding user input to the list
+let addTaskButton = document.querySelector("#addNewTask");
+
+addTaskButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  let text = document.querySelector("#user-input");
+
+  // when Enter is pressed on the keybord the task is added to the list
+  document
+    .querySelector("#addNewTask")
+    .addEventListener("keypress", function (e) {
+      if (e.key === "Enter") {
+        addToDoListItem(text.value);
+      }
+    });
+
+  if (text.value === "") {
+    let popup = `<h1>you didn't type anything :( </h1>`;
+    document.querySelector("#popup").innerHTML = popup;
+    updateHTML();
+  } else {
+    addToDoListItem(text.value);
+    text.value = "";
+    updateHTML();
+  }
+});
+
+//Delete user input to the list
+//let deleteTaskButton = document.querySelectorAll(".delete-item");
+/*console.log(deleteTaskButton);*/
+
+/*deleteTaskButton.forEach((btn) => {
+  btn.addEventListener("click", () => {*/
+/*console.log(typeof parseInt(event.target.id));
+    alert("hello");
+    /*console.log(parseInt(event.target.item.id));
+    deleteItem(parseInt(event.target.item.id));
+    console.log(event.target.item.id);
+    deleteItem(event.target.item.id);
+    updateHTML();
+    console.log(toDoList);
+  });
+});*/
+
+/*const deleteButtons = document.querSelectorAll(".delete-item");
+      deleteButtons.forEach((btn) =>
+        btn.addEventListener("click", (event) => {
+          console.log(typeof parseInt(event.target.id));
+          removeItem(parseInt(event.target.id));
+          updateHTML();
+        })
+      );
+    };*/
 
 let deleteItem = (id) => {
   toDoList = toDoList.filter((item) => item.id !== parseInt(id));
-  console.log(id, toDoList);
+  /*console.log(id, toDoList);*/
 };
 
-deleteItem(3);
+let crossItem = (id) => {
+  /*toDoList = toDoList.filter((item) => item.id !== parseInt(id));*/
+  /*console.log(id, toDoList);*/
+};
 
-console.log(toDoList);
+/*const removeItem = (id) => (tasks = tasks.filter((task) => task.id !== id));*/
 
 // REPLACE ONE THING
 function updateTodoListItem(id, newText) {
-    const index = toDoList.findIndex((item) => {
+  const index = toDoList.findIndex((item) => {
     return item.id === id;
   });
-  toDoList.splice(index, 1, { id: id, text: newText, vegetable: true });
+  toDoList.splice(index, 1, { id: id, text: newText, done: true });
 }
 
-updateTodoListItem(3, "not asparagus");
+/*addToDoListItem("clean bathroom");
+addToDoListItem("finish phd");
+addToDoListItem("watch TV");
+addToDoListItem("finish JavaScript project");
+addToDoListItem("finish JavaScript project");
+addToDoListItem("finish JavaScript project");
+addToDoListItem("finish JavaScript project");
+addToDoListItem("finish JavaScript project");
+addToDoListItem("watch TV");
+addToDoListItem("Drink a glass of wine !!");*/
+
+/*deleteItem(3);
+deleteItem(7);
+deleteItem(5);*/
+/*console.log(toDoList);*/
